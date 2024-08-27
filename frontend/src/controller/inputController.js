11ecbfa0063fafacaa03f630bbd32d11dc7515a4
DongHyeonWon <dhe7700@naver.com>
2024-08-27 17:47:34 +0900

    fix: 아무 입력도 없을 시 toast 메시지 출력

--- frontend/src/controller/inputController.js
+++ frontend/src/controller/inputController.js
@@ -19,7 +19,6 @@ export default class InputController {
     this.input.addEventListener("keydown", ({ key, isComposing }) => {
       if (key === "Enter" && !isComposing) {
         const message = this.input.value.trim();
-        if (message === "") return;
         if (message.length < 6) {
           this.toast.render("메시지는 6자 이상으로 입력해주세요.");
           console.log("메시지는 6자 이상으로 입력해주세요.");
@@ -34,7 +33,6 @@ export default class InputController {
   buttonSubmit() {
     this.button.addEventListener("click", () => {
       const message = this.input.value.trim();
-      if (message === "") return;
       if (message.length < 6) {
         this.toast.render("메시지는 6자 이상으로 입력해주세요.");
         console.log("메시지는 6자 이상으로 입력해주세요.");
